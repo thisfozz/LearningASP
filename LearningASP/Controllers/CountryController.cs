e38a054d882bff0d0dd6ae62dee82e7906cad697
thisfozz <kfcfozz@vk.com>
2024-06-10 15:36:41 +0500

    Пятое задание со странами готово

--- LearningASP/Controllers/CountryController.cs
+++ LearningASP/Controllers/CountryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LearningASP.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LearningASP.Controllers;
 
@@ -9,6 +10,119 @@ public class CountryController : Controller
     [Route("/Homework/HomeWorkOne/CountryDisplayInfo")]
     public IActionResult CountryDisplayInfoOnTr()
     {
-        return View("CountryInfo");
+        var countries = new List<Country>
+        {
+                new Country
+                {
+                    Name = "Абхазия",
+                    FullName = "Республика Абхазия",
+                    NameInEnglish = "Abkhazia",
+                    Alpha2 = "AB",
+                    Alpha3 = "ABH",
+                    ISO = "895",
+                    Location = "Азия, Закавказье"
+                },
+                new Country
+                {
+                    Name = "Австралия",
+                    FullName = "",
+                    NameInEnglish = "Australia",
+                    Alpha2 = "AU",
+                    Alpha3 = "AUS",
+                    ISO = "036",
+                    Location = "Океания, Австралия и Новая Зеландия"
+                },
+                new Country
+                {
+                    Name = "Австрия",
+                    FullName = "Австрийская Республика",
+                    NameInEnglish = "Austria",
+                    Alpha2 = "AT",
+                    Alpha3 = "AUT",
+                    ISO = "040",
+                    Location = "Европа, Западная Европа"
+                },
+                new Country
+                {
+                    Name = "Азербайджан",
+                    FullName = "Республика Азербайджан",
+                    NameInEnglish = "Azerbaijan",
+                    Alpha2 = "AZ",
+                    Alpha3 = "AZE",
+                    ISO = "031",
+                    Location = "Азия, Западная Азия"
+                },
+                new Country
+                {
+                    Name = "Албания",
+                    FullName = "Республика Албания",
+                    NameInEnglish = "Albania",
+                    Alpha2 = "AL",
+                    Alpha3 = "ALB",
+                    ISO = "008",
+                    Location = "Европа, Южная Европа"
+                },
+                new Country
+                {
+                    Name = "Алжир",
+                    FullName = "Алжирская Народная Демократическая Республика",
+                    NameInEnglish = "Algeria",
+                    Alpha2 = "DZ",
+                    Alpha3 = "DZA",
+                    ISO = "012",
+                    Location = "Африка, Северная Африка"
+                },
+                new Country
+                {
+                    Name = "Американское Самоа",
+                    FullName = "",
+                    NameInEnglish = "American Samoa",
+                    Alpha2 = "AS",
+                    Alpha3 = "ASM",
+                    ISO = "016",
+                    Location = "Океания, Полинезия"
+                },
+                new Country
+                {
+                    Name = "Ангилья",
+                    FullName = "",
+                    NameInEnglish = "Anguilla",
+                    Alpha2 = "AI",
+                    Alpha3 = "AIA",
+                    ISO = "660",
+                    Location = "Америка, Карибский бассейн"
+                },
+                new Country
+                {
+                    Name = "Ангола",
+                    FullName = "Республика Ангола",
+                    NameInEnglish = "Angola",
+                    Alpha2 = "AO",
+                    Alpha3 = "AGO",
+                    ISO = "024",
+                    Location = "Африка, Центральная Африка"
+                },
+                new Country
+                {
+                    Name = "Андорра",
+                    FullName = "Княжество Андорра",
+                    NameInEnglish = "Andorra",
+                    Alpha2 = "AD",
+                    Alpha3 = "AND",
+                    ISO = "020",
+                    Location = "Европа,	Южная Европа"
+                },
+                new Country
+                {
+                    Name = "Антарктида",
+                    FullName = "",
+                    NameInEnglish = "Antarctica",
+                    Alpha2 = "AQ",
+                    Alpha3 = "ATA",
+                    ISO = "010",
+                    Location = "Антарктика"
+                },
+        };
+        return View("CountryInfo" ,countries);
     }
 }
---
+++ LearningASP/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace LearningASP.Models;
+
+public class Country
+{
+    public string Name { get; set; }
+    public string FullName { get; set; }
+    public string NameInEnglish { get; set; }
+    public string Alpha2 { get; set; }
+    public string Alpha3 { get; set; }
+    public string ISO { get; set; }
+    public string Location { get; set; }
+}
